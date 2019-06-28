5b366498186d4e6c342595faee7b261dc828f39c
Philipp Strube <pst@kubestack.com>
2019-06-28 22:23:11 +0200

    fixup! Setup pipeline service account in both IAM and K8s

--- google/_modules/gke/pipeline.tf
+++ google/_modules/gke/pipeline.tf
@@ -24,9 +24,9 @@ resource "kubernetes_namespace" "pipeline" {
 
   # namespace metadata may change through the manifests
   # hence ignoring this for the terraform lifecycle
-  #lifecycle {
-  #  ignore_changes = [metadata]
-  #}
+  lifecycle {
+    ignore_changes = [metadata]
+  }
 
   depends_on = [module.node_pool]
 }
