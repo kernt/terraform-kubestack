68458ab865aa18abadf6e07a2bd9da80f02c0718
Philipp Strube <pst@kubestack.com>
2019-07-08 18:33:48 +0200

    fixup! Generate SSH key-pair for read-only repository access from the pipeline

--- google/_modules/gke/outputs.tf
+++ google/_modules/gke/outputs.tf
@@ -3,3 +3,7 @@ output "ingress_zone_name_servers" {
   description = "Nameservers of the cluster's managed zone."
 }
 
+output "pipeline_id_rsa.pub" {
+  value       = tls_private_key.pipeline.public_key_openssh
+  description = "Public key used for the pipeline in OpenSSH format."
+}
